36a0a86ce12cd0c5a819f1629c6d28c42111ce1b
ujisati <>
2023-10-22 14:31:11 -0600

    reorganize ast.rs

--- src/ast.rs
+++ src/ast.rs
@@ -2,50 +2,40 @@ use macros::Node;
 
 use crate::lexer::{Token, TokenType};
 
-#[derive(Node, Debug)]
-pub struct Boolean {
-    pub token: Token,
-    pub value: bool,
+pub trait Node {
+    fn token_literal(&self) -> String;
 }
 
+pub trait DebugString {
+    fn repr(&self) -> String;
+}
+
+#[derive(Debug)]
 pub struct Program {
     pub statements: Vec<Statement>,
 }
 
-impl DebugString for Program {
-    fn repr(&self) -> String {
-        let mut output = String::from("");
-        for s in &self.statements {
-            output.push_str(&s.repr())
-        }
-        output
-    }
-}
-
-pub enum Statement {
-    Let(Let),
-    Return(Return),
-    ExpressionStmt(ExpressionStmt),
-}
-
-impl DebugString for Statement {
-    fn repr(&self) -> String {
-        match self {
-            Self::Let(s) => s.repr(),
-            Self::Return(s) => s.repr(),
-            Self::ExpressionStmt(s) => s.repr(),
-        }
-    }
-}
-
 #[derive(Debug)]
 pub enum Expression {
-    None, //FIXME: remove this
+    None,
     Identifier(Identifier),
     Integer(Integer),
     Prefix(Prefix),
     Infix(Infix),
     Boolean(Boolean),
+    If(If)
+}
+
+#[derive(Node, Debug)]
+pub struct Identifier {
+    pub token: Token,
+    pub value: String,
+}
+
+#[derive(Node, Debug)]
+pub struct Integer {
+    pub token: Token,
+    pub value: i64,
 }
 
 #[derive(Node, Debug)]
@@ -61,6 +51,67 @@ pub struct Infix {
     pub operator: String,
     pub left: Box<Expression>,
     pub right: Box<Expression>,
+}
+
+#[derive(Node, Debug)]
+pub struct Boolean {
+    pub token: Token,
+    pub value: bool,
+}
+
+#[derive(Node, Debug)]
+struct If {
+    token: Token,
+    condition: Box<Expression>,
+    consequence: Program,
+    alternative: Program,
+}
+
+#[derive(Debug)]
+pub enum Statement {
+    Let(Let),
+    Return(Return),
+    ExpressionStmt(ExpressionStmt),
+}
+
+#[derive(Node, Debug)]
+pub struct Let {
+    pub token: Token,
+    pub name: Identifier,
+    pub value: Expression,
+}
+
+#[derive(Node, Debug)]
+pub struct Return {
+    pub token: Token,
+    pub return_value: Expression,
+}
+
+#[derive(Node, Debug)]
+pub struct ExpressionStmt {
+    pub token: Token,
+    pub expression: Expression,
+}
+
+
+impl DebugString for Program {
+    fn repr(&self) -> String {
+        let mut output = String::from("");
+        for s in &self.statements {
+            output.push_str(&s.repr())
+        }
+        output
+    }
+}
+
+impl DebugString for Statement {
+    fn repr(&self) -> String {
+        match self {
+            Self::Let(s) => s.repr(),
+            Self::Return(s) => s.repr(),
+            Self::ExpressionStmt(s) => s.repr(),
+        }
+    }
 }
 
 impl DebugString for Infix {
@@ -101,23 +152,10 @@ impl DebugString for Expression {
     }
 }
 
-#[derive(Node)]
-pub struct ExpressionStmt {
-    pub token: Token,
-    pub expression: Expression,
-}
-
 impl DebugString for ExpressionStmt {
     fn repr(&self) -> String {
         self.expression.repr()
     }
-}
-
-#[derive(Node)]
-pub struct Let {
-    pub token: Token,
-    pub name: Identifier,
-    pub value: Expression,
 }
 
 impl DebugString for Let {
@@ -126,28 +164,10 @@ impl DebugString for Let {
     }
 }
 
-#[derive(Node)]
-pub struct Return {
-    pub token: Token,
-    pub return_value: Expression,
-}
-
 impl DebugString for Return {
     fn repr(&self) -> String {
         self.token_literal() + &self.return_value.repr()
     }
-}
-
-#[derive(Node, Debug)]
-pub struct Identifier {
-    pub token: Token,
-    pub value: String,
-}
-
-#[derive(Node, Debug)]
-pub struct Integer {
-    pub token: Token,
-    pub value: i64,
 }
 
 impl DebugString for Integer {
@@ -168,12 +188,10 @@ impl DebugString for Boolean {
     }
 }
 
-pub trait Node {
-    fn token_literal(&self) -> String;
-}
-
-pub trait DebugString {
-    fn repr(&self) -> String;
+impl DebugString for If {
+    fn repr(&self) -> String {
+        todo!()
+    }
 }
 
 #[cfg(test)]
